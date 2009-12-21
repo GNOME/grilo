557b71dac961b114ad72e233f02e5337e2aaab77
Iago Toral <itoral@igalia.com>
2009-12-21 09:12:56 +0100

    Moved full resolution support to MsMetadataSource

--- src/ms-metadata-source.c
+++ src/ms-metadata-source.c
@@ -21,6 +21,8 @@
  */
 
 #include "ms-metadata-source.h"
+#include "ms-metadata-source-priv.h"
+#include "ms-plugin-registry.h"
 
 #include <string.h>
 
@@ -49,6 +51,17 @@ static void ms_metadata_source_set_property (GObject *object,
                                              guint prop_id,
                                              const GValue *value,
                                              GParamSpec *pspec);
+
+static void
+print_keys (gchar *label, const GList *keys)
+{
+  g_print ("%s: [", label);
+  while (keys) {
+    g_print (" %d", GPOINTER_TO_INT (keys->data));
+    keys = g_list_next (keys);
+  }
+  g_print (" ]\n");
+}
 
 G_DEFINE_ABSTRACT_TYPE (MsMetadataSource, ms_metadata_source, MS_TYPE_MEDIA_PLUGIN);
 
@@ -270,4 +283,155 @@ ms_metadata_source_filter_supported (MsMetadataSource *source, GList **keys)
   }
 
   return filtered_keys;
+}
+
+void
+ms_metadata_source_setup_full_resolution_mode (MsMetadataSource *source,
+					       const GList *keys,
+					       struct SourceKeyMapList *key_mapping)
+{
+  print_keys ("Requested keys", keys);
+  
+  key_mapping->source_maps = NULL;
+  key_mapping->operation_keys = NULL;
+
+  /* key_list holds keys to be resolved */
+  GList *key_list = g_list_copy ((GList *) keys);
+  
+  /* Filter keys supported by this source */
+  key_mapping->operation_keys = 
+    ms_metadata_source_filter_supported (MS_METADATA_SOURCE (source),
+					 &key_list);
+  
+  if (key_list == NULL) {
+    g_debug ("Source supports all requested keys");
+    goto done;
+  }
+  
+  print_keys ("Keys supported in source", key_mapping->operation_keys);
+  
+  /*
+   * 1) Find which sources (other than the current one) can resolve
+   *    some of the missing keys
+   * 2) Check out dependencies for the keys they can resolve 
+   * 3) For each dependency list check if the  original source can resolve them.
+   *    3.1) Yes: Add key and dependencies to be resolved
+   *    3.2) No: forget about that key and its dependencies
+   *         Ideally, we would check if other sources can resolve them
+   * 4) Execute the user operation passing in our own callback
+   * 5) For each result, check the sources that can resolve
+   *    the missing metadata and issue resolution operations on them.
+   *    We could do this once per source passing in a list with all the 
+   *    browse results. Problem is we lose response time although we gain
+   *    overall efficiency.
+   * 6) Invoke user callback with results
+   */
+  
+  /* Find which sources resolve which keys */
+  GList *supported_keys;
+  MsMetadataSource *_source;
+  MsMediaPlugin **source_list;
+  GList *iter;
+  MsPluginRegistry *registry;
+  
+  registry = ms_plugin_registry_get_instance ();
+  source_list = ms_plugin_registry_get_sources (registry);
+  
+  while (*source_list && key_list) {
+    gchar *name;
+    
+    _source = MS_METADATA_SOURCE (*source_list);
+    
+    source_list++;
+    
+    /* Interested in sources other than this  */
+    if (_source == MS_METADATA_SOURCE (source)) {
+      continue;
+    }
+    
+    print_keys ("Unsupported keys", key_list);
+    
+    /* Interested in sources capable of resolving metadata
+       based on other metadata */
+    MsMetadataSourceClass *_source_class =
+      MS_METADATA_SOURCE_GET_CLASS (_source);
+    if (!_source_class->resolve) {
+      continue;
+    }
+    
+    /* Check if this source supports some of the missing keys */
+    g_object_get (_source, "source-name", &name, NULL);
+    g_debug ("Checking resolution capabilities for source '%s'", name);
+    supported_keys = ms_metadata_source_filter_supported (_source, &key_list);
+    
+    if (!supported_keys) {
+      g_debug ("  Source does not support any of the keys, skipping.");
+      continue;
+    }
+    
+    g_debug ("  '%s' can resolve some keys, checking deps", name);
+    print_keys ("Keys supported by source", supported_keys);
+    
+    /* Check the external dependencies for these supported keys */
+    GList *supported_deps;
+    GList *iter_prev;
+    iter = supported_keys;
+    while (iter) {
+      MsKeyID key = GPOINTER_TO_INT (iter->data);
+      GList *deps =
+	g_list_copy ((GList *) ms_metadata_source_key_depends (_source, key));
+      
+      iter_prev = iter;
+      iter = g_list_next (iter);
+      
+      /* deps == NULL means the key cannot be resolved 
+	 by using only metadata */
+      if (!deps) {
+	g_debug ("    Key '%u' cannot be resolved from metadata", key);
+	supported_keys = g_list_delete_link (supported_keys, iter_prev);
+	key_list = g_list_prepend (key_list, GINT_TO_POINTER (key));
+	continue;
+      }
+      g_debug ("    Key '%u' might be resolved using external metadata", key);
+      
+      /* Check if the original source can solve these dependencies */
+      supported_deps = 
+	ms_metadata_source_filter_supported (MS_METADATA_SOURCE (source), 
+					     &deps);
+      if (deps) {
+	g_debug ("      Dependencies not supported by source, dropping key");
+	/* Maybe some other source can still resolve it */
+	/* TODO: maybe some of the sources already inspected could provide
+	   these keys! */
+	supported_keys = 
+	  g_list_delete_link (supported_keys, iter_prev);
+	/* Put the key back in the list, maybe some other soure can
+	   resolve it */
+	key_list = g_list_prepend (key_list, GINT_TO_POINTER (key));
+      } else {
+	g_debug ("      Dependencies supported by source, including key");
+	/* Add these dependencies to the list of keys for
+	   the browse operation */
+	/* TODO: maybe some of these keys are in the list already! */
+	key_mapping->operation_keys =
+	  g_list_concat (key_mapping->operation_keys, supported_deps);
+      }
+    }
+    
+    /* Save the key map for this source */
+    if (supported_keys) {
+      g_debug ("  Adding source '%s' to the resolution map", name);
+      struct SourceKeyMap *source_key_map = g_new (struct SourceKeyMap, 1);
+      source_key_map->source = _source;
+      source_key_map->keys = supported_keys;
+      key_mapping->source_maps =
+	g_list_prepend (key_mapping->source_maps, source_key_map);
+    }
+  }
+
+  if (key_mapping->source_maps == NULL) {
+      g_debug ("No key mapping for other sources, can't resolve more metadata");
+  }
+done:
+  return;
 }
