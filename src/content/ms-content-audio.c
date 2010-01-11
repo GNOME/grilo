777ba0b25bf273a3305402433dba59e9520e3f60
Juan A. Suarez Romero <jasuarez@igalia.com>
2010-01-11 12:29:54 +0100

    Add a new audio content media to store audio-only media.

---
+++ src/content/ms-content-audio.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2010 Igalia S.L.
+ *
+ * Contact: Iago Toral Quiroga <itoral@igalia.com>
+ *
+ * Authors: Juan A. Suarez Romero <jasuarez@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+/*
+ * A multimedia content for aduio.
+ *
+ * This high level class represents an audio multimedia container. It has methods to
+ * set and get properties like album, and so on.
+ *
+ */
+
+#include "ms-content-audio.h"
+
+
+static void ms_content_audio_dispose (GObject *object);
+static void ms_content_audio_finalize (GObject *object);
+
+G_DEFINE_TYPE (MsContentAudio, ms_content_audio, MS_TYPE_CONTENT_MEDIA);
+
+static void
+ms_content_audio_class_init (MsContentAudioClass *klass)
+{
+    GObjectClass *gobject_class = (GObjectClass *)klass;
+
+    gobject_class->dispose = ms_content_audio_dispose;
+    gobject_class->finalize = ms_content_audio_finalize;
+}
+
+static void
+ms_content_audio_init (MsContentAudio *self)
+{
+}
+
+static void
+ms_content_audio_dispose (GObject *object)
+{
+    G_OBJECT_CLASS (ms_content_audio_parent_class)->dispose (object);
+}
+
+static void
+ms_content_audio_finalize (GObject *object)
+{
+    g_signal_handlers_destroy (object);
+    G_OBJECT_CLASS (ms_content_audio_parent_class)->finalize (object);
+}
+
+/**
+ * ms_content_audio_new:
+ *
+ * Creates a new content audio object.
+ *
+ * Returns: a newly-allocated content audio.
+ **/
+MsContentAudio *
+ms_content_audio_new (void)
+{
+  return g_object_new (MS_TYPE_CONTENT_AUDIO,
+		       NULL);
+}
---
+++ src/content/ms-content-audio.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2010 Igalia S.L.
+ *
+ * Contact: Iago Toral Quiroga <itoral@igalia.com>
+ *
+ * Authors: Juan A. Suarez Romero <jasuarez@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef __MS_CONTENT_AUDIO_H__
+#define __MS_CONTENT_AUDIO_H__
+
+#include "ms-content-media.h"
+
+
+G_BEGIN_DECLS
+
+#define MS_TYPE_CONTENT_AUDIO                   \
+  (ms_content_audio_get_type())
+#define MS_CONTENT_AUDIO(obj)                                           \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj),                                   \
+                               MS_TYPE_CONTENT_AUDIO,                   \
+                               MsContentAudio))
+#define MS_CONTENT_AUDIO_CLASS(klass)                                   \
+  (G_TYPE_CHECK_CLASS_CAST ((klass),                                    \
+                            MS_TYPE_CONTENT_AUDIO,                      \
+                            MsContentAudioClass))
+#define IS_MS_CONTENT_AUDIO(obj)                                        \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj),                                   \
+                               MS_TYPE_CONTENT_AUDIO))
+#define IS_MS_CONTENT_AUDIO_CLASS(klass)                                \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass),                                    \
+                            MS_TYPE_CONTENT_AUDIO))
+#define MS_CONTENT_AUDIO_GET_CLASS(obj)                                 \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj),                                    \
+                              MS_TYPE_CONTENT_AUDIO,                    \
+                              MsContentAudioClass))
+
+typedef struct _MsContentAudio      MsContentAudio;
+typedef struct _MsContentAudioClass MsContentAudioClass;
+
+struct _MsContentAudioClass
+{
+    MsContentMediaClass parent_class;
+};
+
+struct _MsContentAudio
+{
+    MsContentMedia parent;
+};
+
+#define ms_content_audio_set_artist(content, artist)                    \
+  ms_content_set_string(MS_CONTENT((content)), MS_METADATA_KEY_ARTIST, (artist))
+#define ms_content_audio_set_album(content, album)                      \
+  ms_content_set_string(MS_CONTENT((content)), MS_METADATA_KEY_ALBUM, (album))
+#define ms_content_audio_set_genre(content, genre)                      \
+  ms_content_set_string(MS_CONTENT((content)), MS_METADATA_KEY_GENRE, (genre))
+#define ms_content_audio_set_lyrics(content, lyrics)                    \
+  ms_content_set_string(MS_CONTENT((content)), MS_METADATA_KEY_LYRICS, (lyrics))
+
+#define ms_content_audio_get_artist(content)                            \
+  ms_content_get_string(MS_CONTENT((content)), MS_METADATA_KEY_ARTIST)
+#define ms_content_audio_get_album(content)                             \
+  ms_content_get_string(MS_CONTENT((content)), MS_METADATA_KEY_ALBUM)
+#define ms_content_audio_get_genre(content)                             \
+  ms_content_get_string(MS_CONTENT((content)), MS_METADATA_KEY_GENRE)
+#define ms_content_audio_get_lyrics(content)                            \
+  ms_content_get_string(MS_CONTENT((content)), MS_METADATA_KEY_LYRICS)
+
+GType ms_content_audio_get_type (void) G_GNUC_CONST;
+MsContentAudio *ms_content_audio_new (void);
+
+G_END_DECLS
+
+#endif /* __CONTENT_AUDIO_H__ */
