4b2da639f807a0b6a8c517d99cfc472b2e47ae1f
Guillaume Emont <gemont@igalia.com>
2011-02-07 20:58:32 +0100

    core: added GRL_CORE_ERROR_OPERATION_CANCELLED
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>

--- src/grl-error.h
+++ src/grl-error.h
@@ -56,6 +56,7 @@
  * @GRL_CORE_ERROR_UNLOAD_PLUGIN_FAILED: Failed to unload plugin
  * @GRL_CORE_ERROR_REGISTER_METADATA_KEY_FAILED: Failed to register metadata key
  * @GRL_CORE_ERROR_NOTIFY_CHANGED_FAILED: Failed to start changed notifications
+ * @GRL_CORE_ERROR_OPERATION_CANCELLED: The operation was cancelled
  *
  * These constants identify all the available core errors
  */
@@ -77,7 +78,8 @@ typedef enum {
   GRL_CORE_ERROR_LOAD_PLUGIN_FAILED,
   GRL_CORE_ERROR_UNLOAD_PLUGIN_FAILED,
   GRL_CORE_ERROR_REGISTER_METADATA_KEY_FAILED,
-  GRL_CORE_ERROR_NOTIFY_CHANGED_FAILED
+  GRL_CORE_ERROR_NOTIFY_CHANGED_FAILED,
+  GRL_CORE_ERROR_OPERATION_CANCELLED
 } GrlCoreError;
 
 #endif /* _GRL_ERROR_H_ */
