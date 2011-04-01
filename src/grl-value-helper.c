8b600784827e547cf312c987c06d6e91c68dc332
Guillaume Emont <gemont@igalia.com>
2011-04-01 16:56:25 +0200

    core: Added some helper functions to handle GValues

---
+++ src/grl-value-helper.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2011 Igalia S.L.
+ *
+ * Contact: Iago Toral Quiroga <itoral@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+/**
+ * SECTION:grl-value-helper
+ * @short_description: helpers to use GValue
+ *
+ * This module provides helper functions to use GValues easily. Inspired by
+ * libsoup's soup-value-utils:
+ * http://git.gnome.org/browse/libsoup/tree/libsoup/soup-value-utils.c
+ *
+ */
+
+#include <grl-value-helper.h>
+
+
+/**
+ * grl_g_value_new: (skip):
+ */
+GValue *
+grl_g_value_new (GType g_type)
+{
+  GValue *value;
+
+  value = g_slice_new0 (GValue);
+  g_value_init (value, g_type);
+
+  return value;
+}
+
+/**
+ * grl_g_value_free: (skip):
+ */
+void
+grl_g_value_free (GValue *value)
+{
+  g_value_unset (value);
+  g_slice_free (GValue, value);
+}
+
+/**
+ * grl_g_value_hashtable_new: (skip):
+ * Returns: a new hash table made to contain GValues.
+ */
+GHashTable *
+grl_g_value_hashtable_new (void)
+{
+  return g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify)grl_g_value_free);
+}
+
+/**
+ * grl_g_value_dup: (skip):
+ */
+GValue *
+grl_g_value_dup (const GValue *value)
+{
+  GValue *new_value = grl_g_value_new (G_VALUE_TYPE (value));
+  g_value_copy (value, new_value);
+
+  return new_value;
+}
