f4ba85cff2f01d8f0e0d6d39ec302f145ceb11de
1PunMan <saurabhsingh412@gmail.com>
2018-06-01 20:11:22 +0530

    what

--- src/data/grl-data.c
+++ src/data/grl-data.c
@@ -634,6 +634,7 @@ is_canonical (const gchar *key)
     (c < 'A' || c > 'Z') &&
     (c < 'a' || c > 'z'))
   return FALSE;
+      else GRL_WARNING("Did not print FALSE");
     }
 
   GRL_WARNING("Returns TRUE");
@@ -659,6 +660,7 @@ grl_data_set_for_id (GrlData *data, const gchar *key_name, const GValue *value)
   GType type = G_TYPE_NONE;
   GParamSpec *spec;
 
+  key_name = g_intern_string (key_name);
   //g_return_val_if_fail (key_name != NULL, NULL);
   //g_return_val_if_fail ((key_name[0] >= 'A' && key_name[0] <= 'Z') || (key_name[0] >= 'a' && key_name[0] <= 'z'), NULL);
   //g_return_val_if_fail (is_canonical (key_name), NULL);
