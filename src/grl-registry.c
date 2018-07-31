f2826aedbfb77ad900f40c09b1ef5685b06f38c3
Marinus Schraal <mschraal@gnome.org>
2018-07-31 10:31:39 +0200

    registry: Glob pattern matching for plugin ranks
    Source names can be constructed partially with unique identifiers, making
    it difficult to match directly to the static identifiers in
    GRL_PLUGIN_RANKS.
    
    Introduce glob pattern matching for GRL_PLUGIN_RANKS, which allows for
    example GRL_PLUGIN_RANKS="grl-lastfm-cover*:3".
    
    Add a short explanation and example to the documentation as well.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=761695

--- src/grl-registry.c
+++ src/grl-registry.c
@@ -380,9 +380,26 @@ set_source_rank (GrlRegistry *registry, GrlSource *source)
   rank =
     GPOINTER_TO_INT (g_hash_table_lookup (registry->priv->ranks,
                                           grl_source_get_id (source)));
+
+  if (!rank) {
+    GHashTableIter iter;
+    const gchar *key = NULL;
+    gpointer value;
+
+    g_hash_table_iter_init(&iter, registry->priv->ranks);
+
+    while (g_hash_table_iter_next (&iter, (gpointer *) &key, &value)) {
+      if (g_pattern_match_simple ((const gchar*) key, grl_source_get_id (source))) {
+        rank = GPOINTER_TO_INT (value);
+        break;
+      }
+    }
+  }
+
   if (!rank) {
     rank = GRL_RANK_DEFAULT;
   }
+
   g_object_set (source, "rank", rank, NULL);
   GRL_DEBUG ("Source rank '%s' : %d", grl_source_get_id (source), rank);
 }
