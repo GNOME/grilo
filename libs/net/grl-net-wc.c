a52459f1321cf621338125d1e928d052a3fd1833
Bastien Nocera <hadess@hadess.net>
2021-08-26 14:44:07 +0200

    net: Fix double-free when using GrlNet in Python
    grl_net_wc_request_finish() claims to have a "transfer full" content
    argument but it actually returns a pointer to a variable inside the
    GrlNetWc object, leading to possible double-frees.

--- libs/net/grl-net-wc.c
+++ libs/net/grl-net-wc.c
@@ -948,7 +948,7 @@ grl_net_wc_request_with_headers_hash_async (GrlNetWc *self,
  * @self: a #GrlNetWc instance
  * @result: The result of the request
  * @content: (out) (array length=length) (element-type guint8) (allow-none)
- * (transfer full): The contents of the resource
+ * (transfer none): The contents of the resource
  * @length: (out) (allow-none): The length of the contents or %NULL if it is not
  * needed
  * @error: return location for a #GError, or %NULL
